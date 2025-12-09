9e3eb45c019335508e11599cd2f9aba83ce55d6c
NikunjkGarg <nikunjkumar@eupotic.io>
2025-12-09 18:10:23 +0530

    react element learning

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, 
+    [React.createElement("h1", {id: "heading"}, "Namastey React"), React.createElement("h1", {id: "heading"}, "Namastey React")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
