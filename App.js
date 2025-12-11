c4fdbab27d6e2f7955b75cb646b05c9e0cf29b02
NikunjkGarg <nikunjkumar@eupotic.io>
2025-12-11 07:57:54 +0530

    introduction to jsx

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, 
-    [React.createElement("h1", {id: "heading"}, "Namastey React"), React.createElement("h1", {id: "heading"}, "Namastey React")]));
+// const heading = React.createElement("h1", {id: "heading"}, "Namastey React");
+
+const Heading = () => { return (<h1 id="heading" className="heading1">Namastey React!!</h1>)}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />)
+
+
