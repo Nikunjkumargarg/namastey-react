75eab424ef11c3e8d0a088a4064d3e580f591cb9
NikunjkGarg <nikunjkumar@eupotic.io>
2025-12-15 11:19:25 +0530

    entry of react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact"
 const AppLayout = () => {
     return (
         <div className="app">
@@ -11,7 +13,22 @@ const AppLayout = () => {
         </div>
     )
 }
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />
+    },
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />)
+root.render(<RouterProvider router={appRouter} />)
 
 
